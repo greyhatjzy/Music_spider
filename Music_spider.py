6ef8a47686a0500c149863512766f10bea2c5c98
greyhatjzy <1435763997@qq.com>
2020-09-08 15:03:33 +0800

    超过20首

--- Music_spider.py
+++ Music_spider.py
@@ -10,31 +10,75 @@ class Music_Download():
 
     def __init__(self, download_dir, count=20):
         self.orig_website = 'http://tool.liumingye.cn/music/?page=searchPage'
+        option = webdriver.ChromeOptions()
+
+        # 设置option,不显示浏览器窗口
+        option.add_argument('headless')
         # self.driver = webdriver.Chrome()
-        self.driver = webdriver.Chrome('E:\Music_spider\driver\chromedriver.exe')
+        # self.driver = webdriver.Chrome(chrome_options=option)
+        # self.driver = webdriver.Chrome('D:\Code\Music_spider\driver\chromedriver.exe', chrome_options=option)
+        self.driver = webdriver.Chrome('D:\Code\Music_spider\driver\chromedriver.exe')
+
         self.driver.implicitly_wait(5)
         self.download_dir = download_dir
         self.count = int(count)
         self.now_time = datetime.datetime.now().strftime('%Y-%m-%d')
 
-    def download_artist(self, artist):
+    def download_artist(self, artist, ifcontinue=False):
         '''
         默认下载20首歌曲，超过20首时，点击 next page
-        :param artist:
-        :return:
+
         '''
 
-        # 从原始页面搜索
-        self.driver.get(self.orig_website)
-        input_tag = self.driver.find_element_by_id('input')
-        input_tag.send_keys(artist)
-        input_tag.send_keys(Keys.ENTER)
-        time.sleep(2)
+        self.search(artist, ifcontinue)
 
         # href是JS生成的，如果直接寻找下载节点，没有href信息
+        # 当超过20首歌时，递归调用自己，节点选择最后20个即可。
         # download_links =self.driver.find_elements_by_css_selector("[class='btn btn-outline-secondary download']")
-        current_page = self.driver.find_elements_by_tag_name("[class='aplayer-list'] li")
 
+        current_page = self.driver.find_elements_by_tag_name("[class='aplayer-list'] li")[-20:]
+        for item in current_page:
+            index, Artist, Music_name, Cover, Urls = self.get_url(item)
+            if index < self.count:
+                print('正在解析第%s首歌：' % index, artist, '-', Music_name)
+                info_json = {'Index': index, 'Artist': Artist, 'Music_name': Music_name, 'Urls': Urls}
+                self.saver(info_json)
+
+                try:
+                    self.downloader(Artist, Music_name, Cover, Urls)
+                except:
+                    print('下载失败')
+
+                if index % 5 == 0:
+                    self.driver.execute_script('window.scrollBy(0,180);')
+
+                if index % 20 == 0:
+                    next_page = self.driver.find_element_by_class_name('aplayer-more')
+                    next_page.click()
+                    time.sleep(1)
+                    self.driver.execute_script('window.scrollBy(0,280);')
+                    self.download_artist(artist, ifcontinue=True)
+            else:
+                break
+        self.driver.quit()
+
+    def search(self, artist, ifcontinue):
+        # TODO: 默认了搜索引擎
+
+        # 从原始页面搜索
+        if ifcontinue:
+            pass
+        else:
+            self.driver.get(self.orig_website)
+            input_tag = self.driver.find_element_by_id('input')
+            input_tag.send_keys(artist)
+            input_tag.send_keys(Keys.ENTER)
+            time.sleep(2)
+
+    def download_rank(self, rank_name):
+        self.driver.get(rank_name)
+
+        current_page = self.driver.find_elements_by_tag_name("[class='aplayer-list'] li")
         count = 0
 
         for item in current_page:
@@ -59,9 +103,6 @@ class Music_Download():
                 next_page.click()
         self.driver.quit()
 
-    def download_rank(self, rank_name):
-        pass
-
     def download_title(self, title):
         '''
         从3个主流媒体中搜索下载top N 的歌曲作为备选
@@ -81,7 +122,7 @@ class Music_Download():
 
             self.saver(info_json)
             try:
-                self.downloader(Artist, Music_name, Cover,Urls)
+                self.downloader(Artist, Music_name, Cover, Urls)
             except:
                 print('下载失败')
             time.sleep(1)
@@ -92,9 +133,8 @@ class Music_Download():
         # Attention: 优化css选择器，减少误选
         artist = item.find_element_by_css_selector("[class='aplayer-list-author']").text
         title = item.find_element_by_css_selector("[class='aplayer-list-title']").text
-        index = item.find_element_by_css_selector("[class='aplayer-list-index']").text
+        index = int(item.find_element_by_css_selector("[class='aplayer-list-index']").text)
 
-        print('正在解析第%s首歌：' % int(index), artist, title)
 
         downloader_icon = item.find_element_by_css_selector(
             "[class='aplayer-list-download iconfont icon-xiazai']")
@@ -114,7 +154,6 @@ class Music_Download():
             "[class='btn btn-outline-secondary pic_download']").get_attribute(
             'href')
 
-        print(cover)
         # 解析完成下载链接之后，要关闭dialog，返回上一级，从而实现遍历
         back = self.driver.find_element_by_css_selector("div>[class='btn btn-primary']")
         time.sleep(1)
@@ -154,21 +193,28 @@ class Music_Download():
 
 
 if __name__ == '__main__':
-    os.chdir(r'E:\Music_spider')
-    count = 20
-    download_dir = r'E:\Music_spider\driver'
-    artists_list = ['新裤子']
+    os.chdir(r'D:\Code\Music_spider')
+    count = 30
+    download_dir = r'D:\Code\Music_spider'
+    artists_list = ['泰勒斯威夫特']
     title_list = [
         'PLANET ラムジ',
         ' Butter-Fly 和田光司',
         ' 名探侦コナン メイン・テーマ(银翼ヴァージョン)	大野克夫'
     ]
+    rank_list = [
+        r'http://tool.liumingye.cn/music/?page=audioPage&type=YQD&name=https%3A%2F%2Fmusic.163.com%2F%23%2Fplaylist%3Fid%3D2829883282']
 
-    # for artist in artists_list:
-    #     downloader = Music_Download(download_dir, count)
-    #     downloader.download_artist(artist)
-    #     print('.................%s...................' % artist, '下载完成')
+    for artist in artists_list:
+        downloader = Music_Download(download_dir, count)
+        downloader.download_artist(artist)
+        print('.................%s...................' % artist, '下载完成')
 
-    for title in title_list:
-        downloader = Music_Download(download_dir, 1)
-        downloader.download_title(title)
+    # for title in title_list:
+    #     downloader = Music_Download(download_dir, 1)
+    #     downloader.download_title(title)
+    #     break
+    #
+    # for rank in rank_list:
+    #     downloader = Music_Download(download_dir, 100)
+    #     downloader.download_rank(rank)
